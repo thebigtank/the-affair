f6ba777b72deb3dd1bd68a80127d6457e508cdbc
Itse Sagay <codeplay.erniie@gmail.com>
2022-08-03 12:12:21 +0100

    refactored mobile-menu toggle state

--- js/components/mobile-menu.js
+++ js/components/mobile-menu.js
@@ -1,9 +1,11 @@
 export default function mobileMenu() {
+	let body = document.querySelector("body");
 	let header = document.querySelector("header");
 	let hamburger = document.querySelector(".hamburger");
 
 
 	hamburger.addEventListener("click", function () {
+		body.classList.toggle("mobile-menu");
 		header.classList.toggle("active");
 	});
 }
