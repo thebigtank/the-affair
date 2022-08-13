b01f3f51d5f614cdfe9effccae2c49cbda3ac759
Itse Sagay <codeplay.erniie@gmail.com>
2022-08-13 02:11:27 +0100

    Added GSAP

---
+++ js/components/hero-animation.js
@@ -0,0 +1,5 @@
+export default function heroAnimation() {
+    let tl = gsap.timeline({defaults: {ease: "power4.inOut", duration: 2}});
+
+    tl.to('h1', {'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)', opacity: 1, y: 0, durationn: 2.2});
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import mobileMenu from "./components/mobile-menu.js";
+import heroAnimation from "./components/hero-animation.js";
 
 document.addEventListener("DOMContentLoaded", function () {
 	mobileMenu();
+	heroAnimation();
 });
